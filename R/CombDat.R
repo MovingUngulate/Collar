0b93e0babd6f9553351a144c8d489372b5986812
Hayes <mhayes1@uwyo.edu>
2018-05-15 20:23:36 -0600

    initial push

---
+++ R/CombDat.R
@@ -0,0 +1,91 @@
+#' @title Combine ATS and Vectronic Data
+#
+#' @description This function combines ATS Iridium data and the csv files downloaded by vectronic GPS Plus X
+#' @param vecpath Path your Vectronic GPS Plus X Program saves CSV files to
+#' @param ATSUsers a character vector of all ATS Iridium login Usernames
+#' @param ATSPass a character vector of all ATS Iridium login passwords
+#' @param ST do you have sirtrack data TRUE/FALSE
+#' @param STUser SirTrack Username
+#' @param STPass SirTrack Password
+#' @param tempdir a temporary directory to store files in when downloading from ATS site
+#' @return Resulting object is a spatial points data frame of your ATS and vectronics data
+#' downloaded using GPS Plus X as a csv
+#' \strong{Spatial Data Column Description:}
+#' \item{CollarSerialNumber}{Collar Serial Number}
+#' \item{TelemDate}{POSIX Field of collare fixes in the USER'S timezone. NOTE: may need to alter timezone}
+#' \item{HDOP}{Horizontal Dilution of Precision}
+#' \item{2D/3D}{Whether fix is a 2d or 3d fix. Values are either 2 or 3}
+#' \item{Temperature}{Temperature reported by collar}
+#' @keywords vectronic, ats
+#' @export
+#' @examples
+#' \donttest{dd<-CombDat(vecpath='F:/Box Sync/DEER/Data/Vectronic/VecData',
+#' ATSUsers=c('user1','user2'),
+#' ATSPass = c('pass1','pass2'),
+#' tempdir='C:/Users/mhayes1/Desktop/temp')}
+#'
+CombDat<-function(vecpath,ATSUsers,ATSPass,tempdir,ST=TRUE,STUser=NULL,STPass=NULL,yourlink){
+
+  if(nchar(vecpath)>0){
+  llist<-list.files(vecpath,
+                    pattern='GPS_Default Storage.csv',full.names=T)
+
+  vdat<-data.frame()
+  for(i in 1:length(llist)){
+    sub<-read.csv(llist[i],stringsAsFactors = F,encoding='latin1')
+    vdat<-rbind(vdat,sub)
+  }
+
+  vdat<-vdat[,c(2,3,4,16,17,48,13,14)]
+  vdat$TelemDate<-as.POSIXct(paste(vdat$UTC_Date,vdat$UTC_Time,sep=' '),'%m/%d/%Y %I:%M:%S %p',tz='UTC')
+
+  attributes(vdat$TelemDate)$tzone<-'MST'
+  vdat<-vdat[,c(1,9,4,5,6,7,8)]
+  names(vdat)<-c('CollarSerialNumber','TelemDate','HDOP','X2D.3D','Temperature','Lat','Long')
+  vdat<-vdat[complete.cases(vdat$Long),]
+  vdat<-vdat[complete.cases(vdat$Lat),]
+
+  
+  vdat<-vdat[order(vdat$TelemDate),]
+  
+  sp::coordinates(vdat)<-~Long+Lat
+  sp::proj4string(vdat)<-'+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs'
+
+}
+  for(i in 1:length(ATSUsers)){
+    de<-Part::ColDownload(username=ATSUsers[i],password=ATSPass[i],dirdown=tempdir,cType='ATS/IRID')
+    #de[[1]]@data$Study<-ATSList[i]
+    
+    if(i == 1){
+      dat2<-de[[2]]
+      dat<-de[[1]]
+    }else{
+      dat<-rbind(dat,de[[1]])
+      dat2<-rbind(dat2,de[[2]])
+    }
+
+  }
+
+  #dat<-de[[1]]
+  dat@data<-dat@data[,c(1,2,3,5,7)]
+  
+  saveRDS(de[[2]],paste0(tempdir,'DDown.RDS'))
+
+  if(nchar(vecpath)>0){
+  jk<-rbind(dat,vdat)
+  }
+  if(nchar(vecpath)==0){
+    jk<-dat
+  }
+
+  if(ST == TRUE){
+  st<-SirTrackDat(user=STUser,pass=STPass,
+                        saveas=paste0(tempdir,'STDat.csv'),yourlink=yourlink)
+
+  
+  jk<-rbind(jk,st)
+  }
+  
+  return(jk)
+
+}
---
+++ README.md
@@ -0,0 +1,80 @@
+# Collar <img src="man/figures/PartLog.jpg" align="right" height="200" width="120" />
+
+## Overview
+
+This package aids in easily downloading GPS collar datasets, and associated data, that reside online (satellite linked collars).
+
+## Installation
+
+### Currently, the only version of part is on GitHub:
+
+To install:
+
+```{r}
+install.packages("devtools")
+
+devtools::install_github("MovingUngulates/Collar")
+```
+
+To uninstall current version and get newest version:
+
+```{r}
+remove.packages("Part", lib="~/R/win-library/3.4")
+
+devtools::install_github('MovingUngulate/Collar')
+```
+
+## General Thoughts
+Currently, there are methods for ATS (global star and Iridium), SirTrack and Vectronics.
+The current methods are simply based on systems that I've had access to over the years. If
+you'd like to add your system please contact me about adding it! Adding new functionality
+is hoped for but will require partnership and access to your specific sites to determine
+how to automate your workflow.
+
+## Usage
+
+ATS Downloads
+
+```{r}
+
+examp<-ColDownload(username = ATSUser,password=ATSPass,
+                        dirdown = tempdir,cType='ATS/IRID')
+
+```
+
+SirTrack Downloads
+To get yourlink, login to your SirTrack site, right click the download entire dataset csv
+link and copy link address. Paste into the function as a character vector. Should only 
+need to do once.
+
+```{r}
+
+examp<-SirTrackDat(user = ATSUser,pass=ATSPass,
+                        saveas = 'C:/Users/mhayes1/Desktop/STDat.csv'
+                        ,yourlink=seehelp)
+
+```
+
+Vectronic Downloads
+
+```{r}
+
+#Ensure that your data is saved as .csv in GPSPlusX!
+examp<-getVec('GPSPlusxpath')
+
+```
+
+Mixture of some ATS, Vectronic and SirTrack Data (if you don't have one or two that ok!)
+
+```{r}
+
+#vecpath is the character path to your vectronic download folder (.csv!)
+#ATSUsers/ATSPass is the character string of ATS Usernames/Passwords (can do #c('acct1','acct2') if you have more than one!)
+#tempdir is a temporary storage folder
+#ST is TRUE/FALSE if you have sirtrack data
+#If ST == TRUE, STUser and STPass are the username/password for sirtrack site
+#yourlink is the yourlink described above
+
+examp<- CombDat(vecpath,ATSUsers,ATSPass,
+               tempdir=tempdir,ST,STUser,STPass,yourlink)
+```
